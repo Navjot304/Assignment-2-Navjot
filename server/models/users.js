28ebf74f818909a09c2b044be06abded2bfe49ae
Navjot Kaur <navjot304@gmail.com>
2017-04-19 17:00:19 -0400

    Users update

--- server/models/users.js
+++ server/models/users.js
@@ -5,44 +5,53 @@ File Name: users.js
 Web app name: https://comp308-2017-midterm-300869349.herokuapp.com/
 */
 
-// require modules for our User Model
-let mongoose = require('mongoose');
-let Schema = mongoose.Schema; //alias for mongoose Schema
-let passportLocalMongoose = require('passport-local-mongoose');
+// require these models for our user model
+let mongoose = require("mongoose");
+let Schema = mongoose.Schema;
+let passportLocalMongoose = require("passport-local-mongoose");
 
-let UserSchema = new Schema({
-  username: {
-    type: String,
-    default: '',
-    trim: true,
-    required: 'username is required'
-  },
-  email: {
-    type: String,
-    default: '',
-    trim: true,
-    required: 'email is required'
-  },
-  displayName: {
-    type: String,
-    default: '',
-    trim: true,
-    required: 'Display Name is required'
-  },
-  created: {
-    type: Date,
-    default: Date.now
-  },
-  updated: {
-    type: Date,
-    default: Date.now
-  }
-},{
-  collection: "users"
+
+let UserScema = new Schema ({
+    username : {
+      type : String,
+      default : '',
+      trim : true,
+      required : 'username is required'      
+    },
+    password : {
+      type : String,
+      default : '',
+      trim : true,
+      required : 'password is required'      
+    },
+    email : {
+    type : String,
+      default : '',
+      trim : true,
+      required : 'email is required'      
+    },
+    displayName : {
+        type : String,
+      default : '',
+      trim : true,
+      required : 'Display Name is required'
+    },
+    created: {
+        type : Date,
+        default : Date.now
+    },
+    updated: {
+        type : Date,
+        default : Date.now
+    }
+},
+{
+    collection : "users"
 });
 
-let options = ({missingPasswordError: "Wrong Password"});
 
-UserSchema.plugin(passportLocalMongoose, options);  
+let options = ({missingPassswordError : "Wrong Password"});
 
-exports.User = mongoose.model('User', UserSchema);
+UserScema.plugin(passportLocalMongoose , options);
+
+exports.User = mongoose.model('User',UserScema);
